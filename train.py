f90e6751364586211868a0800eb419d882857bf9
Oscar Dadfar <cardadfar@gmail.com>
2021-10-26 00:28:00 -0400

    added working catmul rom/ broken bspline

--- train.py
+++ train.py
@@ -8,6 +8,8 @@ from PIL import Image as im
 from dataset import NPYDataset
 from model import Network
 
+EPS_F = 1e-7
+
 parser = argparse.ArgumentParser(
         description='Main function to call training for different AutoEncoders')
 parser.add_argument('--batch-size', type=int, default=128, metavar='N',
@@ -83,7 +85,7 @@ def save_model(model, loss_fn, opt, epoch):
     print("Model saved at epoch {0}.".format(epoch))
 
 def load_model(model, loss_fn, opt, epoch):
-    checkpoint = torch.load("checkpoints/model_epoch_" + str(epoch))
+    checkpoint = torch.load("checkpoints/model_epoch_" + str(epoch), map_location=device)
     model.load_state_dict(checkpoint['model_state_dict'])
     opt.load_state_dict(checkpoint['optimizer_state_dict'])
     epoch = checkpoint['epoch']
@@ -132,8 +134,12 @@ def train(model, data_loader, loss_fn, opt, epoch):
         pred.save('plots/train/pred_epoch_' + str(epoch) + '.png')
 
 
-def sigmoid(x, scale=10.0):
+def sigmoid(x, scale=15.0):
     return 1.0 / (1.0 + np.exp(scale * (-x + 0.5)))
+
+
+def iden(x):
+    return x
 
 def save_img(data, name, test=True):
     true_data = data.detach().cpu().numpy()
@@ -143,24 +149,102 @@ def save_img(data, name, test=True):
     else:
         true.save('plots/train/' + name)
 
-def linear(x, ib=1):
+def B(x, k, i, t):
+    if k == 0:
+       return 1.0 if torch.all((t[i] <= x) == True) and torch.all((x < t[i+1]) == True) else 0.0
+    c1 = (x - t[i])/(t[i+k] - t[i] + EPS_F) * B(x, k-1, i, t)
+    c2 = (t[i+k+1] - x)/(t[i+k+1] - t[i+1] + EPS_F) * B(x, k-1, i+1, t)
+    return c1 + c2
+
+def bspline_interp(x, t, c, k):
+    #https://docs.scipy.org/doc/scipy/reference/generated/scipy.interpolate.BSpline.html
+    n = len(t) - k - 1
+    assert (n >= k+1) and (len(c) >= n)
+    return sum(c[i] * B(x, k, i, t) for i in range(n))
+
+def bspline(x, ibf=1, ease=iden):
     '''
     linearly interpolate between frames
     x : encoded tensor (n, encode_dim)
-    ib : inbetween frame num
+    ibf : inbetween frame num
     '''
 
     n, encode_dim = x.shape
 
-
-    y = torch.Tensor((n-1)*(ib+1)+1, encode_dim)
+    y = torch.Tensor((n-1)*(ibf+1)+1, encode_dim)
     idx = 0
     for i in range(n-1):
         y[idx] = x[i]
         idx += 1
-        for t in range(1,ib+1):
-            wgt = t / (ib + 2.0)
-            ibtwn = (1.0 - wgt) * x[i] + wgt * x[i+1]
+        for f in range(1,ibf+1):
+            t = ease(f / (ibf + 2.0))
+            xt = linear_interp(x[i], x[i+1], t)
+            k = 2
+            c = np.zeros(n - k - 1)
+            c[0] = -1
+            c[-1] = 1
+            ibtwn = bspline_interp(xt, x, c, k)
+            y[idx] = ibtwn
+            idx += 1
+    
+    y[idx] = x[n-1]
+    return y
+
+def catmullRom(x, ibf=1, ease=iden):
+    '''
+    linearly interpolate between frames
+    x : encoded tensor (n, encode_dim)
+    ibf : inbetween frame num
+    '''
+
+    n, encode_dim = x.shape
+
+    y = torch.Tensor((n-1)*(ibf+1)+1, encode_dim)
+    idx = 0
+    for i in range(n-1):
+        y[idx] = x[i]
+        idx += 1
+        for f in range(1,ibf+1):
+            t = ease(f / (ibf + 2.0))
+            if i == 0:
+                ibtwn = catmullRom_interp(x[i], x[i], x[i+1], x[i+2], t)
+            elif i == n - 2:
+                ibtwn = catmullRom_interp(x[i-1], x[i], x[i+1], x[i+1], t)
+            else:
+                ibtwn = catmullRom_interp(x[i-1], x[i], x[i+1], x[i+2], t)
+            y[idx] = ibtwn
+            idx += 1
+    
+    y[idx] = x[n-1]
+    return y
+
+def linear_interp(x0, x1, t):
+    return (1.0 - t) * x0 + t * x1
+
+def catmullRom_interp(x0, x1, x2, x3, t):
+    # https://www.mvps.org/directx/articles/catmull/
+    return  0.5 * ((2*x1) +\
+                    (-x0 + x2) * t +\
+                    (2*x0 - 5*x1 + 4*x2 - x3) * t**2 +\
+                    (-x0 + 3*x1- 3*x2 + x3) * t**3)
+
+def linear(x, ibf=1, ease=iden):
+    '''
+    linearly interpolate between frames
+    x : encoded tensor (n, encode_dim)
+    ibf : inbetween frame num
+    '''
+
+    n, encode_dim = x.shape
+
+    y = torch.Tensor((n-1)*(ibf+1)+1, encode_dim)
+    idx = 0
+    for i in range(n-1):
+        y[idx] = x[i]
+        idx += 1
+        for f in range(1,ibf+1):
+            t = ease(f / (ibf + 2.0))
+            ibtwn = linear_interp(x[i], x[i+1], t)
             y[idx] = ibtwn
             idx += 1
     
@@ -180,33 +264,20 @@ def test(model, data_loader, loss_fn):
         avg_loss += loss.item()
         avg_loss_cnt += 1
 
-        #TODO: simple way to print images. Maybe I can make this look nicer...
         if batch_idx == 0:
-            encoded = model.encode(data[0:5].view(-1, 784))
+            encoded = model.encode(data[0:10].view(-1, 784))
 
-            '''
-            save_img(data[0], '0.0.png')
-            
-            for j in range(4):
-                for i in range(0,7):
-                    wgt = sigmoid(i / 7.0, 5)
-                    encoded_blend = (1.0 - wgt) * encoded[j] + wgt * encoded[j+1]
-
-                    output = model.decode(encoded_blend.view(1, 32))
-
-                    pred_data = F.sigmoid(8*(output[0]-0.5)) * 255
-                    save_img(pred_data, str(j) + '.' + str(i) + '.png')
-            
-                save_img(data[j+1], str(j+1) + '.0.png')
-            '''
-
-            y = linear(encoded)
+            #y = linear(encoded, ibf=5, ease=sigmoid)
+            y = catmullRom(encoded, ibf=5, ease=iden)
+            #y = bspline(encoded, ibf=5, ease=iden)
             y = y.to(device)
             output = model.decode(y)
-            output = F.sigmoid(8*(output-0.5)) * 255
+            output = torch.sigmoid(8*(output-0.5)) * 255
 
             for i in range(len(output)):
                 save_img(output[i], str(i) + '.png')
+            
+            return
 
 
             
